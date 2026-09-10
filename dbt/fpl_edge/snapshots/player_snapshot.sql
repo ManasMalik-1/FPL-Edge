56b459f1e0525102b8a9aa5f75273c2570fbac64
Manas Malik <YOUR_GITHUB_EMAIL@example.com>
2026-09-10 19:42:41 +0530

    Add SCD Type 2 player snapshot

---
+++ dbt/fpl_edge/snapshots/player_snapshot.sql
@@ -0,0 +1,20 @@
+{% snapshot player_snapshot %}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key='player_id',
+        strategy='check',
+        check_cols=[
+            'price',
+            'selected_by_percent',
+            'status',
+            'chance_of_playing_next_round'
+        ]
+    )
+}}
+
+select *
+from {{ ref('stg_fpl__players') }}
+
+{% endsnapshot %}
